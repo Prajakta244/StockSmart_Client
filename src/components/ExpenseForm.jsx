adfe08cc2778a5d2cf091f2be07a0236ec3ef1f7
prajakta shinde <prajakta2498shinde@gmail.com>
2024-11-24 20:23:30 +0530

    add form validations

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Grid from '@mui/material/Grid2';
-import { TextField, Button,Typography, useTheme } from '@mui/material';
+import { TextField, Button, Typography, useTheme } from '@mui/material';
 import { useState, useEffect } from 'react';
 import FlexBetween from './FlexBetween';
 import { useGetExpenseQuery, usePostExpenseMutation } from 'state/api';
@@ -11,7 +11,8 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import 'dayjs/locale/de';
 import { format } from 'date-fns'
-
+import * as Yup from 'yup'
+import dayjs from 'dayjs';
 
 const ExpenseForm = ({ closeForm }) => {
   const theme = useTheme();
@@ -25,9 +26,19 @@ const ExpenseForm = ({ closeForm }) => {
   });
   const { vertical, horizontal, open } = isSnackBarOpen;
   const { data, isLoading, error, refetch } = useGetExpenseQuery();
+  const [validationErrorObj, setValidationErr] = useState({
+    description: '',
+    amount: '',
+    date: ''
+  })
+  const validationSchema = Yup.object({
+    description: Yup.string().matches(/^(?=.*[a-zA-Z])[a-zA-Z0-9\s]+$/, "Only alphanumeric characters are allowed.").required('Required'),
+    amount: Yup.number().typeError('Amount must be a number.').required('Required'),
+    date: Yup.date().required()
+  })
   const [formData, setFormData] = useState({
     amount: 0,
-    date: '',
+    date: dayjs(new Date()).format('YYYY-MM-DD'),
     description: ''
   });
   const [postExpense] = usePostExpenseMutation();
@@ -36,6 +47,7 @@ const ExpenseForm = ({ closeForm }) => {
     try {
       event.preventDefault();
       const expenseData = formData;
+      const validations = await validationSchema.validate(expenseData, { abortEarly: false })
       let msg = ''
       console.log('expense data', expenseData)
       const res = await postExpense(expenseData)
@@ -54,13 +66,28 @@ const ExpenseForm = ({ closeForm }) => {
         Transition: SlideTransition,
       })
     } catch (e) {
+      let msg = 'Something went wrong!!! Try again later.'
+      setSnackBarOpen({
+        ...isSnackBarOpen,
+        open: true,
+        Transition: SlideTransition,
+      })
+      if (e?.inner) {
+        msg = 'Validation failed. Please check the form fields and correct any errors.'
+        const errObj = {}
+        e.inner.forEach(errorObj => {
+          errObj[errorObj['path']] = errorObj.errors[0]
+        })
+        setValidationErr(errObj)
+      }
+      setMessage(msg)
       console.error(`Error occured while adding transaction ${e}`)
     }
   };
 
 
   return (
-        <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <Snackbar
         open={isSnackBarOpen.open}
         anchorOrigin={{ vertical, horizontal }}
@@ -69,109 +96,117 @@ const ExpenseForm = ({ closeForm }) => {
         key={vertical + horizontal}
         autoHideDuration={1200}
       />
-        <FlexBetween sx={{ m: 5 }}>
-          <Grid container spacing={2}>
-            <Grid
-              size={12}
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
+      <FlexBetween sx={{ m: 5 }}>
+        <Grid container spacing={2}>
+          <Grid
+            size={12}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              sx={{ fontSize: 30 }}
+              color={theme.palette.secondary[400]}
+              gutterBottom
             >
-              <Typography
-                sx={{ fontSize: 30 }}
-                color={theme.palette.secondary[400]}
-                gutterBottom
-              >
-                Add Expense
-              </Typography>
-            </Grid>
-            <Grid
-              size={6}
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <TextField
-                type="text"
-                variant="outlined"
-                label="Description"
-                value={formData.description}
-                onChange={(e) =>
-                  setFormData({ ...formData, description: e.target.value })
-                }
-              ></TextField>
-            </Grid>
-            <Grid
-              size={6}
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <TextField
-                type="text"
-                variant="outlined"
-                label="Amount"
-                value={formData.amount}
-                onChange={(e) =>
-                  setFormData({ ...formData, amount: e.target.value })
-                }
-              ></TextField>
-            </Grid>
-            <Grid
-              size={5}
-              sx={{
-                display: "flex",
-                ml: '20px',
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de">
-                <DatePicker format="DD/MM/YYYY" onChange={(date) => setFormData({ ...formData, date: format(date['$d'], 'yyyy-MM-dd') })} />
-              </LocalizationProvider>
-
-            </Grid>
-            <Grid
-              size={6}
-              sx={{
-                display: "flex",
-                justifyContent: "left",
-                alignItems: "center",
-              }}
-            ></Grid>
-            <Grid
-              size={6}
-              sx={{
-                display: "flex",
-                justifyContent: "right",
-                alignItems: "center",
-              }}
-            >
-              <Button variant="outlined" color="secondary" type="submit">
-                Save
-              </Button>
-            </Grid>
-            <Grid
-              size={6}
-              sx={{
-                display: "flex",
-                justifyContent: "left",
-                alignItems: "center",
-              }}
-            >
-              <Button variant="outlined" color="secondary" onClick={closeForm}>
-                Close
-              </Button>
-            </Grid>
+              Add Expense
+            </Typography>
           </Grid>
-        </FlexBetween>
-      </form>
+          <Grid
+            size={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              error={validationErrorObj.description}
+              type="text"
+              variant="outlined"
+              label="Description"
+              value={formData.description}
+              onChange={(e) => {
+                setFormData({ ...formData, description: e.target.value })
+                setValidationErr({ ...validationErrorObj, description: '' })
+              }
+              }
+              helperText={validationErrorObj.description}
+            ></TextField>
+          </Grid>
+          <Grid
+            size={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              error={validationErrorObj.amount}
+              type="text"
+              variant="outlined"
+              label="Amount"
+              value={formData.amount}
+              onChange={(e) => {
+                setFormData({ ...formData, amount: e.target.value })
+                setValidationErr({ ...validationErrorObj, amount: '' })
+              }
+              }
+              helperText={validationErrorObj.amount}
+            ></TextField>
+          </Grid>
+          <Grid
+            size={5}
+            sx={{
+              display: "flex",
+              ml: '20px',
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de">
+              <DatePicker defaultValue={dayjs(new Date())} error={validationErrorObj.date} format="DD/MM/YYYY" onChange={(date) => setFormData({ ...formData, date: format(date['$d'], 'yyyy-MM-dd') })} />
+            </LocalizationProvider>
+
+          </Grid>
+          <Grid
+            size={6}
+            sx={{
+              display: "flex",
+              justifyContent: "left",
+              alignItems: "center",
+            }}
+          ></Grid>
+          <Grid
+            size={6}
+            sx={{
+              display: "flex",
+              justifyContent: "right",
+              alignItems: "center",
+            }}
+          >
+            <Button variant="outlined" color="secondary" type="submit">
+              Save
+            </Button>
+          </Grid>
+          <Grid
+            size={6}
+            sx={{
+              display: "flex",
+              justifyContent: "left",
+              alignItems: "center",
+            }}
+          >
+            <Button variant="outlined" color="secondary" onClick={closeForm}>
+              Close
+            </Button>
+          </Grid>
+        </Grid>
+      </FlexBetween>
+    </form>
   );
 }
 
--- src/components/UseForm.jsx
+++ src/components/UseForm.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Grid from '@mui/material/Grid2';
-import { FormControl,Box } from '@mui/material';
-import {TextField,Button,FormLabel,NativeSelect,Select,MenuItem,InputLabel,useTheme,Typography} from '@mui/material';
-import { useState,useEffect } from 'react';
+import { FormControl, Box } from '@mui/material';
+import { TextField, Button, FormLabel, NativeSelect, Select, MenuItem, InputLabel, useTheme, Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
 import FlexBetween from './FlexBetween';
 import { usePostProductMutation } from 'state/api';
 import { setProduct } from 'state'
-import { useGetProductQuery,useUpdateProductMutation } from 'state/api';
+import { useGetProductQuery, useUpdateProductMutation } from 'state/api';
 import { useDispatch } from 'react-redux'
 import { SnackbarProvider, useSnackbar } from 'notistack';
 import Slide from '@mui/material/Slide';
 import Snackbar from '@mui/material/Snackbar';
+import * as Yup from 'yup'
 
-const UseForm = ({closeForm,action,productData,title}) => {
+const UseForm = ({ closeForm, action, productData, title }) => {
   const theme = useTheme();
   const dispatch = useDispatch()
   const { enqueueSnackbar } = useSnackbar();
@@ -22,79 +23,128 @@ const UseForm = ({closeForm,action,productData,title}) => {
   const [isSnackBarOpen, setSnackBarOpen] = useState({
     open: false,
     Transition: Slide,
-    vertical: 'top', horizontal: 'right' 
+    vertical: 'top', horizontal: 'right'
   });
   const { vertical, horizontal, open } = isSnackBarOpen;
-  const { data, isLoading,error,refetch } = useGetProductQuery();
+  const { data, isLoading, error, refetch } = useGetProductQuery();
   const [formData, setFormData] = useState({
     name: productData?.name || "",
-    quantity:productData?.quantity || 0,
-    unit_price: productData?.unit_price ||0,
-    cost_price: productData?.cost_price ||0,
-    supplier_name: productData?.supplier_name ||"",
-    measure_unit: productData?.measure_unit ||"",
-    measure:productData?.measure ||0,
-    id:productData?.id||''
+    quantity: productData?.quantity,
+    unit_price: productData?.unit_price,
+    cost_price: productData?.cost_price,
+    supplier_name: productData?.supplier_name || "",
+    measure_unit: productData?.measure_unit || "",
+    measure: productData?.measure,
+    id: productData?.id || ''
   });
   console.log(formData.measure_unit)
   const [postProduct] = usePostProductMutation();
-  const[updateProduct] = useUpdateProductMutation()
+  const [updateProduct] = useUpdateProductMutation()
   const unitList = process.env.REACT_APP_UNIT?.split(',')
   console.log("action", action);
   const [message, setMessage] = useState('');
+  const [validationErrorObj, setValidationErr] = useState({
+    name: '',
+    quantity: '',
+    unit_price: '',
+    cost_price: '',
+    supplier_name: '',
+    measure_unit: '',
+    measure: ''
+  })
+  const validationSchema = Yup.object({
+    name: Yup.string().matches(/^(?=.*[a-zA-Z])[a-zA-Z0-9\s]+$/, "Only alphanumeric characters are allowed.").required('Required'),
+    quantity: Yup.number().typeError('Quantity must be a number.').required('Required'),
+    unit_price: Yup.number().typeError('Unit price must be a number.').required('Required'),
+    cost_price: Yup.number().typeError('Cost price must be a number.').required('Required'),
+    supplier_name: Yup.string().matches(/^(?=.*[a-zA-Z])[a-zA-Z0-9\s]+$/, "Only alphanumeric characters are allowed.").required('Required'),
+    measure_unit: Yup.string().required('Required'),
+    measure: Yup.string().required('Required')
+  })
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    try {
+      event.preventDefault();
+      console.log(formData)
+      const validations = await validationSchema.validate(formData, { abortEarly: false })
+      // console.log(`validation ${validations}`)
+      const productData = formData;
+      if (action == 'update') {
+        console.log('prodct data', productData)
+        const res = await updateProduct({ id: productData.id, body: productData })
 
-    const productData = formData;
-    if(action == 'update'){
-      console.log('prodct data',productData)
-      const res = await updateProduct({id:productData.id,body:productData})
-      
-      if(res?.data?.status == 'success'){
-        setMessage('Product updated successfuly!!')
-        refetch()
-        // closeForm()
-        setSnackBarOpen({
-          ...isSnackBarOpen,
-          open: true,
-          Transition:SlideTransition,
-        })
-      }
-    }
-    else
-    {
-      postProduct(productData)
-        .unwrap()
-        .then(() => {
-          console.log('enqueueSnackbar')
-          setMessage('Product added successfuly!!')
+        if (res?.data?.status == 'success') {
+          setMessage('Product updated successfuly!!')
+          refetch()
+          // closeForm()
           setSnackBarOpen({
             ...isSnackBarOpen,
             open: true,
-            Transition:SlideTransition,
+            Transition: SlideTransition,
           })
-          refetch();
-        })
-        .catch((e) => alert(e));
+        }
       }
-    console.log("formData " + formData);
+      else {
+        postProduct(productData)
+          .unwrap()
+          .then(() => {
+            console.log('enqueueSnackbar')
+            setMessage('Product added successfuly!!')
+            setSnackBarOpen({
+              ...isSnackBarOpen,
+              open: true,
+              Transition: SlideTransition,
+            })
+            refetch();
+          })
+          .catch((e) => alert(e));
+      }
+      console.log("formData " + formData);
+    } catch (e) {
+      let msg = 'Something went wrong!!! Try again later.'
+      setSnackBarOpen({
+        ...isSnackBarOpen,
+        open: true,
+        Transition: SlideTransition,
+      })
+      console.error(`Error occured while adding product`)
+      if (e?.inner) {
+        msg = 'Validation failed. Please check the form fields and correct any errors.'
+        const errObj = {}
+        e.inner.forEach(errorObj => {
+          // console.log(errorObj['path'],errorObj.errors[0])
+          errObj[errorObj['path']] = errorObj.errors[0]
+        })
+        console.log(errObj)
+        setValidationErr(errObj)
+        console.log(validationErrorObj)
+      }
+      setMessage(msg)
+    }
+
     // closeForm()
   };
-  
+
 
   return (
     <form onSubmit={handleSubmit}>
       <Snackbar
-          open={isSnackBarOpen.open}
-          anchorOrigin={{ vertical, horizontal }}
-          TransitionComponent={isSnackBarOpen.Transition}
-          message={message}
-          key={vertical + horizontal}
-          autoHideDuration={1200}
-        />
+        open={isSnackBarOpen.open}
+        anchorOrigin={{ vertical, horizontal }}
+        TransitionComponent={isSnackBarOpen.Transition}
+        message={message}
+        key={vertical + horizontal}
+        autoHideDuration={5000}
+        onClose={() => {
+          setSnackBarOpen({
+            ...isSnackBarOpen,
+            open: false,
+            Transition: SlideTransition,
+          })
+        }}
+      />
       <FlexBetween sx={{ m: 5 }}>
-      <Grid container spacing={2}>
-      <Grid
+        <Grid container spacing={2}>
+          <Grid
             size={12}
             sx={{
               display: "flex",
@@ -119,13 +169,17 @@ const UseForm = ({closeForm,action,productData,title}) => {
             }}
           >
             <TextField
+              error={validationErrorObj.name}
               type="text"
               variant="outlined"
               label="Product Name"
               value={formData.name}
-              onChange={(e) =>
+              onChange={(e) => {
                 setFormData({ ...formData, name: e.target.value })
+                setValidationErr({ ...validationErrorObj, name: '' })
               }
+              }
+              helperText={validationErrorObj.name ? validationErrorObj.name : ''}
             ></TextField>
           </Grid>
           <Grid
@@ -137,83 +191,97 @@ const UseForm = ({closeForm,action,productData,title}) => {
             }}
           >
             <TextField
+              error={validationErrorObj.quantity}
               type="text"
               variant="outlined"
               label="Quantity"
               value={formData.quantity}
-              onChange={(e) =>
+              onChange={(e) => {
                 setFormData({ ...formData, quantity: e.target.value })
+                setValidationErr({ ...validationErrorObj, quantity: '' })
               }
+              }
+              helperText={validationErrorObj.quantity ? validationErrorObj.quantity : ''}
             ></TextField>
           </Grid>
           <Grid
             size={3}
             sx={{
               display: "flex",
-              width:'103px',
-              ml:'19px',
+              width: '103px',
+              ml: '19px',
               justifyContent: "center",
               alignItems: "center",
             }}
           >
             <TextField
+              error={validationErrorObj.measure}
               type="text"
               variant="outlined"
               label="Measure"
               value={formData.measure}
-              onChange={(e) =>
+              onChange={(e) => {
                 setFormData({ ...formData, measure: e.target.value })
+                setValidationErr({ ...validationErrorObj, measure: '' })
               }
+              }
+              helperText={validationErrorObj.measure ? validationErrorObj.measure : ''}
             ></TextField>
           </Grid>
           <Grid
             size={3}
             sx={{
               display: "flex",
-              width:'95px',
+              width: '95px',
               justifyContent: "center",
               alignItems: "center",
             }}
           >
             <FormControl>
-            <InputLabel id="unit-select-label">Unit</InputLabel>
-            <Select
-          labelId="unit-select-label"
-          id="unit-select"
-          label="Unit"
-          sx={{width:'88px'}}
-          value={formData.measure_unit}
-          onChange={(e) =>{
-            // alert(e.target.value)
-            setFormData({ ...formData, measure_unit: e.target.value })
-          }
-          }
-        >
-          {unitList && unitList.map(unit => (
-            
-          <MenuItem value={unit}>{unit}</MenuItem>
-          
-          ))}
-        </Select>
-        </FormControl>
+              <InputLabel id="unit-select-label">Unit</InputLabel>
+              <Select
+                error={validationErrorObj.measure_unit}
+                labelId="unit-select-label"
+                id="unit-select"
+                label="Unit"
+                sx={{ width: '88px' }}
+                value={formData.measure_unit}
+                onChange={(e) => {
+                  setFormData({ ...formData, measure_unit: e.target.value })
+                  setValidationErr({ ...validationErrorObj, measure_unit: '' })
+                }
+                }
+                helperText={validationErrorObj.measure_unit ? validationErrorObj.measure_unit : ''}
+              >
+                {unitList && unitList.map(unit => (
+
+                  <MenuItem value={unit}>{unit}</MenuItem>
+
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
           <Grid
             size={6}
             sx={{
               display: "flex",
               justifyContent: "center",
-              ml:'19px',
+              ml: '19px',
               alignItems: "center",
             }}
           >
             <TextField
+              error={validationErrorObj.unit_price}
               type="text"
               variant="outlined"
               label="Price/Unit"
               value={formData.unit_price}
-              onChange={(e) =>
+              onChange={(e) => {
                 setFormData({ ...formData, unit_price: e.target.value })
+                setValidationErr({ ...validationErrorObj, unit_price: '' })
               }
+              }
+              helperText={validationErrorObj.unit_price ? validationErrorObj.unit_price : ''}
             ></TextField>
           </Grid>
           <Grid
@@ -225,13 +293,17 @@ const UseForm = ({closeForm,action,productData,title}) => {
             }}
           >
             <TextField
+              error={validationErrorObj.cost_price}
               type="text"
               variant="outlined"
               label="Cost/Unit"
               value={formData.cost_price}
-              onChange={(e) =>
+              onChange={(e) => {
                 setFormData({ ...formData, cost_price: e.target.value })
+                setValidationErr({ ...validationErrorObj, cost_price: '' })
               }
+              }
+              helperText={validationErrorObj.cost_price ? validationErrorObj.cost_price : ''}
             ></TextField>
           </Grid>
           <Grid
@@ -243,13 +315,17 @@ const UseForm = ({closeForm,action,productData,title}) => {
             }}
           >
             <TextField
+              error={validationErrorObj.supplier_name}
               type="text"
               variant="outlined"
               label="Supplier Name"
               value={formData.supplier_name}
-              onChange={(e) =>
+              onChange={(e) => {
                 setFormData({ ...formData, supplier_name: e.target.value })
+                setValidationErr({ ...validationErrorObj, supplier_name: '' })
               }
+              }
+              helperText={validationErrorObj.supplier_name ? validationErrorObj.supplier_name : ''}
             ></TextField>
           </Grid>
           <Grid
